eebb16c827e0e28044970dfa803b67566dd30f9f
Roman Jędras <romanjedras@o2.pl>
2019-07-14 14:56:01 +0200

    Extensions schem Tour

--- model/tourModel.js
+++ model/tourModel.js
@@ -5,16 +5,53 @@ const tourSchema = new Schema({
   name: {
     type: String,
     required: [true,'A tour must have a name'],
-    unique: true },
-  rating: {
+    unique: true,
+    trim: true
+  },
+  duration: {
+    type: Number,
+    required: [true,'A tour must have a duration']
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true,'A tour must have a group size']
+  },
+  difficulty: {
+    type: String,
+    required: [true,'A tour must have a difficulty']
+  },
+  ratingAverage: {
     type: Number,
     default: 4.5
+  },
+  ratingQuantity: {
+    type: Number,
+    default: 0
   },
   price: {
     type: Number,
     required: [true,'A tour must have a price']
   },
-  created_at: Date,
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    required: [true,'A tour must have a summary']
+  },
+  description: {
+   type: String,
+    trim: true
+  },
+  imageCover: {
+    type: String,
+    required: [true,'A tour must have a iamge cover']
+  },
+  images: [String],
+  created_at: {
+    type: Date,
+    default: Date.now()
+  },
+  startDates: [Date],
   updated_at: Date
 });
 
